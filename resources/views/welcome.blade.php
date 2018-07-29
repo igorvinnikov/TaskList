4496118524a5707a8004667affe8c735fc2e5a77
igorvinnikov <vinnikov92@mail.ru>
2018-07-29 19:56:53 +0300

    change title in welcome template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    TaskList
                 </div>
 
                 <div class="links">
