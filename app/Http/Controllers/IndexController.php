ce5ab0a685d2d1a42af8a8e29cc3c8b81e953000
igorvinnikov <vinnikov92@mail.ru>
2018-07-30 22:54:30 +0300

    add PHPDoc's. Create method to show task lis template

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\View\View;
 
+/**
+ * Class IndexController
+ *
+ * @package App\Http\Controllers
+ */
 class IndexController extends Controller
 {
-    public function show()
+    /**
+     * @return View
+     */
+    public function show() : View
     {
-
+        return view('app.taskList');
     }
 }
