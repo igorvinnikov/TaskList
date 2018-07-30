a088fb560632f67a07852a99878a170e34a21a50
igorvinnikov <vinnikov92@mail.ru>
2018-07-30 15:54:39 +0300

    add route for Index Controller

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,6 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', 'IndexController@show');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('/api/v1/task', 'Rest\TaskController');
