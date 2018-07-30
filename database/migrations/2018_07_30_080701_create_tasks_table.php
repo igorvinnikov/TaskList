b7b7a3ac233b15cfdc93091e4bb470b155d53fcd
igorvinnikov <vinnikov92@mail.ru>
2018-07-30 12:33:38 +0300

    create migration

---
+++ database/migrations/2018_07_30_080701_create_tasks_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement('
+              CREATE TABLE IF NOT EXISTS `tasks` (
+                  `id` INT NOT NULL AUTO_INCREMENT,
+                  `title` VARCHAR(45) NOT NULL,
+                  `description` TEXT NULL,
+                  `creation_date` DATETIME NOT NULL,
+                  `status` ENUM(\'open\', \'closed\') NOT NULL,
+                  PRIMARY KEY (`id`)
+              )
+            DEFAULT CHARACTER SET utf8
+            DEFAULT COLLATE utf8_general_ci
+            ENGINE = InnoDB;
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
